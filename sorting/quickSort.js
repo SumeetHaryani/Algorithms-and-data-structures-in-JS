de7eb2ce5f7faec67de5932a8b335c5a6819cffe
SumeetHaryani <sumeetharyani3@gmail.com>
2020-05-09 23:13:27 +0530

    add quickSort solution

---
+++ sorting/quickSort.js
@@ -0,0 +1,34 @@
+/**
+ * Quick sort
+ * Time Complexity (Average case) - O(n log n)
+ * Space Complexity - O(log n)
+ */
+function partition(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // assuming the pivot is always the first element
+  const pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = partition(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+console.log(quickSort([14,8,7, 6, 3]));
