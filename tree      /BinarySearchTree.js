12e042cec874dc882a692ce434225150cb6666cd
SumeetHaryani <sumeetharyani3@gmail.com>
2020-05-18 12:22:59 +0530

    add BinarySearchTree solution

---
+++ tree      /BinarySearchTree.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current !== null) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+const tree = new BinarySearchTree();
+tree.insert(55);
+tree.insert(65);
+tree.insert(10);
+tree.insert(68);
+tree.insert(20);
+
+
+console.log('tree->', tree.find(20));
+console.log('tree->',tree);
